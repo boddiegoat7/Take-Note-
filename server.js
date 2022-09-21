40d59d7a33e78051a3b84cdc2e74a4be386edc72
victor boddie <victorboddie21@gmail.com>
2022-09-20 20:09:40 -0400

    secound commit

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+const path = require("path");
 const express = require("express");
-const app = express();
+const exphbs = require("express-handlebars");
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
-});
+const routes = require("./controllers");
+//const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join('./', "public")));
+
+app.use(routes);
+
+
+// sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+//});
