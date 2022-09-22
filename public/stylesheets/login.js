f159744201b4de1009b4b957a7ef2790008ad0e5
Youseph <youssoupha.ag@gmail.com>
2022-09-21 21:08:38 -0400

    updated login.js

---
+++ public/stylesheets/login.js
@@ -0,0 +1,44 @@
+function sighnupFormHandler(event) {
+    event.preventDevault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            header: { 'Content-Type': 'application/json' }
+        }).then((response) => {console.log(response)})
+    }
+}
+
+document.querySelector('signup-form').addEventListener('submit', sighnupFormHandler);
+
+const login = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: '{"email":"nwestnedge0@cbc.ca","password":"password123"}'
+  };
+  
+  fetch('/api/users/login', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    const signup = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: '{"username":"luffyboy","email":"neastnedge0@cbc.ca","password":"password333"}'
+      };
+      
+      fetch('/api/users/', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
