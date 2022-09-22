d419272373a2d11cf61cd882e0602bfba30e3ff0
Youseph <youssoupha.ag@gmail.com>
2022-09-22 12:49:36 -0400

    completed login.js front end

--- public/stylesheets/login.js
+++ public/stylesheets/login.js
@@ -1,14 +1,14 @@
 // function login(signupFormHandler)
 
-function signupFormHandler(event) {
-    event.preventDevault();
+async function signupFormHandler(event) {
+    event.preventDefault();
 
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
-        fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
@@ -16,11 +16,42 @@ function signupFormHandler(event) {
                 password
             }),
             header: { 'Content-Type': 'application/json' }
-        }).then((response) => {console.log(response)})
+        });
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
 const login = {
     method: 'POST',
