3296b5de6cad581ed611af88d3b4cde0404c2f10
Youseph <youssoupha.ag@gmail.com>
2022-09-22 19:06:37 -0400

    final commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
     res.render("homepage");
 });
 
--- server.js
+++ server.js
@@ -14,6 +14,23 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
+const session =require('express-session');
+const sequelize = require("./config/connection");
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
@@ -45,6 +62,8 @@ app.post('/api/login', ({ body }, res) => {
     res.status(400).json({ error: errors });
   }
 });
+
+
 
 
 // sequelize.sync({ force: false }).then(() => {
