dc1a3313ac8fc905661c71a363d036480dc0cf01
Yousopp <yousephagueye@gmail.com>
2022-10-13 21:18:21 -0400

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const inputCheck = require('./utils/inputCheck');
 const mysql = require('mysql2');
 
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
+// const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
